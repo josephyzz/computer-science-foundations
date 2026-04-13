10026795afa7a0e7bd958f2f9ed4fbf4ee4572f9
Miguel José <144273867+josephyzz@users.noreply.github.com>
2026-04-13 09:28:03 -0300

    fix(binary_search): fixed function

--- algorithms/searching/binary_search.py
+++ algorithms/searching/binary_search.py
@@ -9,22 +9,24 @@ alphabet = list(string.ascii_lowercase)  # is ordered list
 
 def binary_search(letter: str, alphabet_list: list) -> int | None:
     """
-    Search letter and return the position or null.
+    Search letter and return the position or None.
     Ex: a > b = False, a < b = True
     """
     alphabet = alphabet_list
-    is_continue = True
-    while is_continue:
-        choice = (len(alphabet) // 2) - 1
-        if alphabet[choice] > letter:
-            alphabet = alphabet[:choice]
+    start, end = 0, len(alphabet) - 1
 
-        elif alphabet[choice] < letter:
-            alphabet = alphabet[choice:]
+    while start <= end:
+        middle = (start + end) // 2
+
+        if alphabet[middle] == letter:
+            return middle
+
+        elif alphabet[middle] < letter:
+            start = middle + 1
 
         else:
-            return alphabet.index(alphabet[choice])
+            end = middle - 1
     return
 
 
-print(binary_search("d", alphabet))
+print(binary_search("h", alphabet))
