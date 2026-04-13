287eb221e5fbf55dad5606f03072aad4fd1797d1
Miguel José <144273867+josephyzz@users.noreply.github.com>
2026-04-13 09:35:58 -0300

    feat(binary_search.py): generalizing function

--- algorithms/searching/binary_search.py
+++ algorithms/searching/binary_search.py
@@ -7,25 +7,34 @@ import string
 alphabet = list(string.ascii_lowercase)  # is ordered list
 
 
-def binary_search(letter: str, alphabet_list: list) -> int | None:
+def binary_search(item: str | int, item_list: list) -> int | None:
     """
-    Search letter and return the position or None.
-    Ex: a > b = False, a < b = True
+    Perform a binary search on a sorted list.
+
+    Args:
+        item (str | int): The target value to search for.
+        item_list (list): A sorted list of comparable elements (e.g., letters or numbers).
+
+    Returns:
+        int | None: The index of the target if found, otherwise None.
+
+    Notes:
+        - The input list must be sorted.
+        - Works with any comparable types (e.g., str, int).
     """
-    alphabet = alphabet_list
-    start, end = 0, len(alphabet) - 1
+    start, end = 0, len(item_list) - 1
 
     while start <= end:
         middle = (start + end) // 2
 
-        if alphabet[middle] == letter:
+        if item_list[middle] == item:
             return middle
 
-        elif alphabet[middle] < letter:
-            start = middle + 1
+        elif item_list[middle] < item:
+            start = middle + 1  # Go right
 
         else:
-            end = middle - 1
+            end = middle - 1  # Gp left
     return
 
 
