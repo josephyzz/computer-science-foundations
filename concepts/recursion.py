f9a149d6b961db227b556170e4cc8258c12c8287
josephyzz <miguelneto275@gmail.com>
2026-05-09 21:05:06 -0300

    added fiborncci recursion fuction

--- concepts/recursion.py
+++ concepts/recursion.py
@@ -13,4 +13,22 @@ def factorial(n: int):
         return n * factorial(n - 1)
 
 
+def fibornacci(rabbits: int):
+    rabbits = 1
+    next = 0
+    for i in range(n):
+        print(next)
+        next, rabbits = rabbits + next, next
+
+
 print(factorial(5))  # result = 120
+
+
+def fibornacci(n: int):
+    if n <= 1:
+        return n
+    return fibornacci(n - 1) + fibornacci(n - 2)
+
+
+# result [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+print([fibornacci(i) for i in range(10)])
